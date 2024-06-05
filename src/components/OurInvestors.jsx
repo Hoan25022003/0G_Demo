ae589f381534a574151f5e4561f3de5219c12299
= <thuan140603@gmail.com>
2024-06-05 15:19:07 +0700

    Change width

--- src/components/OurInvestors.jsx
+++ src/components/OurInvestors.jsx
@@ -1,9 +1,22 @@
 const OurInvestors = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center mt-2 gap-x-5 lg:mt-3">
-      <div className="w-[60%] md:w-[50%] grid grid-row gap-y-5 md:flex items-center md:flex-cols mt-5 gap-x-3 md:gap-x-8">
-        <img className="w-full" src="hackvc.svg" alt="" />
-        <img className="w-full" src="blank_less.svg" alt="" />
+      <div className="w-[45%] sm:w-[55%] items-center grid grid-row gap-y-5 lg:w-[50%] md:flex md:flex-cols mt-5 gap-x-3 md:gap-x-8">
+        <div className="w-full flex justify-center">
+          <img
+            className="w-[85%] sm:w-[90%] md:w-full"
+            src="hackvc.svg"
+            alt=""
+          />
+        </div>
+
+        <div className="w-full flex justify-center">
+          <img
+            className="w-[80%] sm:w-[90%] md:w-full"
+            src="blank_less.svg"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
